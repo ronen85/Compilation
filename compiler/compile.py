242662d1b41a2759a700d8f74d5154b0d27fd944
ronen85 <ronen.nizri@gmail.com>
2017-09-02 19:16:13 +0300

    make folder for drink domain and continue work on compile.py

--- compiler/compile.py
+++ compiler/compile.py
@@ -11,14 +11,29 @@ import itertools
 from copy import deepcopy
 from compilation import *
 
+
+# test : python compile.py  ../expfiles/drink/drink-world2.pddl ../expfiles/drink/drink-prob2.pddl ../expfiles/drink/agentsfile.txt ../expfiles/drink/waitfile.txt
+
 def main():
-	if len(sys.argv) < 2:
-		print("Usage: pddl.py <domain> <problem>")
-		return
 	domainfile = sys.argv[1]
 	problemfile = sys.argv[2]
-	(dom,prob) = pddl.parseDomainAndProblem(domainfile, problemfile)
-	MakeDomain(dom, prob, agentslist, waitlist, print_condition)
+	agentsfile = sys.argv[3]
+	waitfile = sys.argv[4]
+	agentslist = []
+	with open(agentsfile) as f:
+		for line in f:
+			agentslist.append(line.rstrip())
+	print agentslist
+	waitlist = []
+	with open(waitfile) as f:
+		for line in f:
+			waitlist.append(line.rstrip())
+	print waitlist
+	# if len(sys.argv) < 2:
+	# 	print("Usage: pddl.py <domain> <problem>")
+	# 	return
+	# (dom,prob) = pddl.parseDomainAndProblem(domainfile, problemfile)
+	# MakeDomain(dom, prob, agentslist, waitlist, print_condition)
 	return
 
 if __name__ == "__main__":
